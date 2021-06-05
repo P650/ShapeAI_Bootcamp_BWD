cfce32b7054489c06389c92336517fbd749f89e8
P650 <63405501+P650@users.noreply.github.com>
2021-06-05 17:51:35 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h3> Javascript and React.js </h3>
+      <p></p>
+      <p>
+        These 7 days bootcamp will explains through basics about Javascript,
+        HTML and React which was conducted by shaurya sinha. We covered
+        everything from scratch and its quite useful session.
+      </p>
+      <p> </p>
+      <p>Thankyou for giving me this opprtunity to attend the session.</p>
+    </div>
+  );
+}
+
+export default Note;
